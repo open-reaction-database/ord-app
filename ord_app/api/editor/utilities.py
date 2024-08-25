3812d85eb04b169d54e85effe0ff57c3aac61767
Steven Kearnes <skearnes@gmail.com>
2024-08-24 23:03:19 -0400

    utilities tests

--- ord_app/api/editor/utilities.py
+++ ord_app/api/editor/utilities.py
@@ -18,11 +18,12 @@ import os
 from base64 import b64decode
 from io import BytesIO
 
-from fastapi import APIRouter, Request, Response
+from fastapi import APIRouter, Request, Response, UploadFile
 from ord_schema import resolvers
 from ord_schema.message_helpers import create_message
 from ord_schema.templating import generate_dataset, read_spreadsheet
 from ord_schema.validations import ValidationOptions, validate_message
+from pydantic import BaseModel
 
 from ord_app.api import send_message
 from ord_app.api.database import add_dataset, get_cursor
@@ -54,7 +55,7 @@ async def validate(message_type: str, request: Request):
     return {"errors": errors, "warnings": warnings}
 
 
-@router.post("/resolve_input")
+@router.get("/resolve_input")
 async def resolve_input(input_string: str):
     """Resolves an input string into a ReactionInput message."""
     try:
@@ -63,17 +64,24 @@ async def resolve_input(input_string: str):
         return Response(str(error), status_code=400)
 
 
+class ResolveCompoundInputs(BaseModel):
+    """Inputs for resolve_compound."""
+
+    identifier_type: str
+    identifier: str
+
+
 @router.post("/resolve_compound")
-async def resolve_compound(identifier_type: str, compound_string: str):
+async def resolve_compound(inputs: ResolveCompoundInputs):
     """Resolves a compound identifier into a SMILES string."""
     try:
-        smiles, resolver = resolvers.name_resolve(identifier_type, compound_string)
+        smiles, resolver = resolvers.name_resolve(inputs.identifier_type, inputs.identifier)
         return {"smiles": resolvers.canonicalize_smiles(smiles), "resolver": resolver}
     except ValueError as error:
         return Response(str(error), status_code=400)
 
 
-@router.post("/canonicalize_smiles")
+@router.get("/canonicalize_smiles")
 async def canonicalize_smiles(smiles: str):
     """Canonicalizes a SMILES string."""
     try:
@@ -82,31 +90,21 @@ async def canonicalize_smiles(smiles: str):
         return Response(str(error), status_code=400)
 
 
-@router.post("/enumerate_dataset")
-async def enumerate_dataset(user_id: str, spreadsheet_name: str, spreadsheet_data: str, template_string: str):
-    """Creates a new dataset based on a template reaction and a spreadsheet.
-
-    A new dataset is created from the template and spreadsheet using ord_schema.templating.generate_dataset.
-
-    Args:
-        user_id: User ID.
-        spreadsheet_name: The original filename of the uploaded spreadsheet.
-        spreadsheet_data: A base64-encoded string containing the contents of the spreadsheet.
-        template_string: A string containing a text-formatted Reaction proto, i.e., the contents of a txtpb file.
-    """
+@router.post("/enumerate_dataset/{user_id}")
+async def enumerate_dataset(user_id: str, template: UploadFile, spreadsheet: UploadFile):
+    """Creates a new dataset based on a template reaction and a spreadsheet."""
     try:
-        basename, suffix = os.path.splitext(spreadsheet_name)
-        spreadsheet_data = BytesIO(b64decode(spreadsheet_data))
-        dataframe = read_spreadsheet(spreadsheet_data, suffix=suffix)
-        name = f"{basename}_dataset"
+        basename, suffix = os.path.splitext(os.path.basename(spreadsheet.filename))
+        dataframe = read_spreadsheet(BytesIO(await spreadsheet.read()), suffix=suffix)
         dataset = generate_dataset(
-            name=name,
+            name=basename,
             description="Enumerated by the ORD editor.",
-            template_string=template_string,
+            template_string=(await template.read()).decode(),
             df=dataframe,
             validate=False,
         )
         with get_cursor() as cursor:
             add_dataset(user_id, dataset, cursor)
+        return basename
     except Exception as error:  # pylint: disable=broad-except
         return Response(str(error), status_code=400)
--- ord_app/api/editor/utilities_test.py
+++ ord_app/api/editor/utilities_test.py
@@ -13,23 +13,67 @@
 # limitations under the License.
 
 """Tests for ord_app.api.editor.utilities."""
+import os
+from base64 import b64decode
+
+import pytest
+from ord_schema.proto.dataset_pb2 import Dataset
+from ord_schema.proto.reaction_pb2 import Percentage, ReactionInput
+
+from ord_app.api.testing import TEST_USER_ID
 
 
-def test_validate():
-    pass
+@pytest.mark.parametrize(
+    "message,expected_num_errors,expected_num_warnings",
+    ((Percentage(value=15.6), 0, 0), (Percentage(precision=-15.6), 2, 0)),
+)
+def test_validate(test_client, message, expected_num_errors, expected_num_warnings):
+    response = test_client.post(f"/api/editor/validate/{message.DESCRIPTOR.name}", data=message.SerializeToString())
+    response.raise_for_status()
+    output = response.json()
+    assert len(output["errors"]) == expected_num_errors
+    assert len(output["warnings"]) == expected_num_warnings
 
 
-def test_resolve_input():
-    pass
+def test_resolve_input(test_client):
+    response = test_client.get(
+        f"/api/editor/resolve_input", params={"input_string": "100 mL of 5.0uM sodium hydroxide in water"}
+    )
+    response.raise_for_status()
+    reaction_input = ReactionInput.FromString(b64decode(response.json()))
+    assert reaction_input.components[0].identifiers[0].value == "sodium hydroxide"
 
 
-def test_resolve_compound():
-    pass
+@pytest.mark.parametrize("identifier_type,data,expected", (("NAME", "benzene", "c1ccccc1"),))
+def test_resolve_compound(test_client, identifier_type, data, expected):
+    response = test_client.post(
+        f"/api/editor/resolve_compound", json={"identifier_type": identifier_type, "identifier": data}
+    )
+    response.raise_for_status()
+    assert response.json()["smiles"] == expected
 
 
-def test_canonicalize_smiles():
-    pass
+@pytest.mark.parametrize("smiles,expected", (("C1=CC=CC=C1", "c1ccccc1"),))
+def test_canonicalize_smiles(test_client, smiles, expected):
+    response = test_client.get(f"/api/editor/canonicalize_smiles", params={"smiles": smiles})
+    response.raise_for_status()
+    assert response.json() == expected
 
 
-def test_enumerate_dataset():
-    pass
+def test_enumerate_dataset(test_client):
+    root = os.path.join(os.path.dirname(__file__), "testdata", "enumeration")
+    template = os.path.join(root, "nielsen_fig1_template.txtpb")
+    spreadsheet = os.path.join(root, "nielsen_fig1.csv")
+    response = test_client.post(
+        f"/api/editor/enumerate_dataset/{TEST_USER_ID}",
+        files={"template": (template, open(template, "rb")), "spreadsheet": (spreadsheet, open(spreadsheet, "rb"))},
+    )
+    response.raise_for_status()
+    dataset_name = response.json()
+    assert dataset_name == "nielsen_fig1"
+    response = test_client.get(
+        "/api/editor/fetch_dataset", params={"user_id": TEST_USER_ID, "dataset_name": dataset_name}
+    )
+    response.raise_for_status()
+    dataset = Dataset.FromString(b64decode(response.json()))
+    assert len(dataset.reactions) == 80
