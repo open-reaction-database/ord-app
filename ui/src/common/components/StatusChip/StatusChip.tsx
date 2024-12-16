adc36ce71e3262761c1ba3c9a5b0c274b469571f
Daniil Sloboda <daniil_sloboda@epam.com>
2024-12-16 18:09:24 +0400

    Updated redux setup

--- ui/src/common/components/StatusChip/StatusChip.tsx
+++ ui/src/common/components/StatusChip/StatusChip.tsx
@@ -16,6 +16,8 @@
 import { Chip } from '@mantine/core';
 import { DATASET_STATUS } from '../../model/datasetStatus';
 import classes from './StatusChip.module.scss';
+import { getDataset } from '../../../store/datasets/datasets.thunks.ts';
+import { useAppDispatch } from '../../../store/useAppDispatch.ts';
 
 interface StatusChipProps {
   status: DATASET_STATUS;
@@ -31,7 +33,9 @@ const statusToColorMapping = new Map<DATASET_STATUS, string>([
 ]);
 
 export function StatusChip({ status }: Readonly<StatusChipProps>) {
+  const dispatch = useAppDispatch();
   const chipColor = statusToColorMapping.get(status);
+  dispatch(getDataset(2));
 
   return (
     <Chip
