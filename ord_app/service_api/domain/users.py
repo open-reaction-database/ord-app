98b35d0c0b38a4e35f6d97ffd5fb794f5ddcde24
Alexander Lopatin <lopatin.alexander.y@gmail.com>
2025-01-09 17:55:41 +0400

    add fix

--- ord_app/service_api/domain/users.py
+++ ord_app/service_api/domain/users.py
@@ -82,7 +82,7 @@ async def jit_provisioning(db_session: AsyncSession, payload: Auth0CreateSchema)
     )
     user = UserModel(**user_payload.model_dump(exclude_unset=True))
     group = GroupModel(name="default", owner=user)
-    group_member = UserGroupsMembershipModel(user=user, group=group, role="admin")
+    group_member = UserGroupsMembershipModel(user_id=user.id, group_id=group.id, role="admin")
 
     db_session.add_all([user, group, group_member])
     await db_session.commit()
