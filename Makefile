11163a9d24eb194cae64b15c6c772b62fd7d1ffc
Oleksandr Lopatin <lopatin.alexander.y@gmail.com>
2025-01-14 13:05:19 +0400

    feature/ORD-11.1 role management (#146)

--- Makefile
+++ Makefile
@@ -1,24 +1,12 @@
-check-black:
-	black --check .
-
-format-black:
-	black .
-
-check-isort:
-	isort --check .
-
-format-isort:
-	isort .
-
-check-pylint:
-	pylint ord_app *.py --fail-under=5.0
+check-ruff:
+	ruff check ord_app/
 
 check-pytype:
 	pytype -j auto
 
 .PHONY: formating
-python-formating: format-black format-isort
+python-formating:
+	ruff check ord_app/ --fix
 
 .PHONY: checking
-python-checking: check-black check-isort check-pylint check-pytype
-
+python-checking: check-ruff check-pytype
--- ord_app/service_api/domain/groups.py
+++ ord_app/service_api/domain/groups.py
@@ -13,99 +13,68 @@
 # limitations under the License.
 from typing import Sequence
 
-from sqlalchemy import case, delete, select, update
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from ord_app.service_api.domain.auth import authenticate
 from ord_app.service_api.models import GroupModel, UserGroupsMembershipModel, UserModel
-from ord_app.service_api.schemas.groups import GroupCreateSchema, GroupMemberCreateSchema, GroupMemberRemoveSchema
+from ord_app.service_api.repositories.groups import GroupMembersRepository, GroupRepository
+from ord_app.service_api.schemas.groups import GroupCreateSchema, GroupMemberEditSchema
+from ord_app.service_api.services.postgresql import get_db_session
 
 
-async def create_group(db_session: AsyncSession, user: UserModel, payload: GroupCreateSchema):
-    # TODO: add response
-    stmt = GroupModel(owner=user, **payload.model_dump(exclude_unset=True))
-    user_group_member = UserGroupsMembershipModel(user=user, group=stmt, role="admin")
-    db_session.add_all([stmt, user_group_member])
-    await db_session.commit()
-    # await db_session.refresh(stmt)
+class GroupUseCases:
+    def __init__(self, db: AsyncSession, current_user: UserModel):
+        self.db = db
+        self.current_user = current_user
+        self.group_repository = GroupRepository(db)
+
+    async def create(self, payload: GroupCreateSchema):
+        return await self.group_repository.create(self.current_user.id, payload.model_dump(exclude_unset=True))
+
+    async def get(self, group_id: int) -> GroupModel:
+        return await self.group_repository.get(group_id)
+
+    async def user_groups(self) -> Sequence[GroupModel]:
+        return await self.group_repository.get_user_groups(self.current_user.id)
+
+    async def update(self, group_id: int, payload: GroupCreateSchema) -> GroupModel:
+        return await self.group_repository.update(group_id, payload.model_dump(exclude_unset=True))
+
+    async def delete(self, group_id: int):
+        await self.group_repository.delete(group_id)
 
 
-async def add_group_members(
-    db_session: AsyncSession, user: UserModel, group_id: int, payload: GroupMemberCreateSchema
-) -> None:
-    group_members = []
-    for member in payload.members:
-        group_members.append(UserGroupsMembershipModel(user_id=member.user_id, group_id=group_id, role=member.role))
-    db_session.add_all(group_members)
-    await db_session.commit()
+class GroupMembersUseCases:
+    def __init__(self, db: AsyncSession = Depends(get_db_session), current_user: UserModel = Depends(authenticate)):
+        self.db = db
+        self.current_user = current_user
+        self.group_members_repository = GroupMembersRepository(db)
+
+    async def all(self, group_id: int) -> Sequence[UserGroupsMembershipModel]:
+        return await self.group_members_repository.all(group_id)
+
+    async def upsert(self, group_id: int, payload: list[GroupMemberEditSchema]):
+        insert_values = [{"user_id": member.user_id, "group_id": group_id, "role": member.role} for member in payload]
+        await self.group_members_repository.upsert(insert_values)
+
+    async def remove_members(self, group_id: int, members_ids: list[int]):
+        await self.group_members_repository.remove_members(group_id, members_ids)
 
 
-async def remove_group_members(
-    db_session: AsyncSession, user: UserModel, group_id: int, payload: GroupMemberRemoveSchema
-) -> None:
-
-    stmt = delete(UserGroupsMembershipModel).where(
-        UserGroupsMembershipModel.user_id.in_(payload.user_ids),
-        UserGroupsMembershipModel.group_id == group_id,
-    )
-    await db_session.execute(stmt)
-    await db_session.commit()
-
-
-async def update_group_members(
-    db_session: AsyncSession, user: UserModel, group_id: int, payload: GroupMemberCreateSchema
-) -> None:
-    # TODO: check if it will work
-    role_mapping = case(
-        {item["user_id"]: item["role"] for item in payload.members},
-        else_=None,
-    )
-
-    stmt = (
-        update(UserGroupsMembershipModel)
-        .where(
-            UserGroupsMembershipModel.group_id == group_id,
-            UserGroupsMembershipModel.user_id.in_([item["user_id"] for item in payload.members]),
-        )
-        .values(role=role_mapping)
-    )
-
-    await db_session.execute(stmt)
-    await db_session.commit()
+def get_group_use_case(
+    db: AsyncSession = Depends(get_db_session),
+    current_user: UserModel = Depends(authenticate),
+) -> GroupUseCases:
+    """
+    A factory function that retrieves `db` and `current_user` via Depends,
+    and then returns a fully initialized UseCase without any mention of Depends inside the UseCase itself.
+    """
+    return GroupUseCases(db=db, current_user=current_user)
 
 
-async def list_groups(db_session: AsyncSession, user: UserModel) -> Sequence[GroupModel]:
-    # TODO: groups where user is member
-    stmt = (
-        select(GroupModel)
-        .join(UserGroupsMembershipModel, UserGroupsMembershipModel.group_id == GroupModel.id)
-        .where(UserGroupsMembershipModel.user == user)
-    )
-    groups = await db_session.scalars(stmt)
-    return groups.all()
-
-
-async def get_group(db_session: AsyncSession, user: UserModel, group_id: int) -> GroupModel:
-    query = select(GroupModel).where(GroupModel.owner == user, GroupModel.id == group_id).limit(1)
-    group = await db_session.scalar(query)
-    return group
-
-
-async def update_group(
-    db_session: AsyncSession, user: UserModel, group_id: int, payload: GroupCreateSchema
-) -> GroupModel:
-    query = (
-        update(GroupModel)
-        .where(GroupModel.id == group_id, GroupModel.owner == user)
-        .values(payload.model_dump())
-        .returning(GroupModel)
-    )
-    result = await db_session.execute(query)
-    await db_session.commit()
-    updated_group = result.scalar_one_or_none()
-    return updated_group
-
-
-async def delete_group(db_session: AsyncSession, user: UserModel, group_id: int):
-    query = delete(GroupModel).where(GroupModel.id == group_id)
-    await db_session.execute(query)
-    await db_session.commit()
+def get_group_members_use_case(
+    db: AsyncSession = Depends(get_db_session),
+    current_user: UserModel = Depends(authenticate),
+) -> GroupMembersUseCases:
+    return GroupMembersUseCases(db=db, current_user=current_user)
