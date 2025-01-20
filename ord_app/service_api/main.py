b31a950a730ed79ed6dd2afa0e520827557ea404
Oleksandr Lopatin <lopatin.alexander.y@gmail.com>
2025-01-20 15:23:43 +0400

    expose auth header and add create empty reaction test (#175)
    * expose auth header and add create empty reaction test
    
    * add Content-Disposition

--- ord_app/service_api/main.py
+++ ord_app/service_api/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["Content-Disposition"]
 )
 
 editor = APIRouter(prefix="/api/v1")
