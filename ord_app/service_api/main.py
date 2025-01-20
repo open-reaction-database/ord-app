84d0049b744fdad4d3388a7968c093505e6cfe7a
Oleksandr Lopatin <lopatin.alexander.y@gmail.com>
2025-01-20 11:27:13 +0100

    expose auth header and add create empty reaction test

--- ord_app/service_api/main.py
+++ ord_app/service_api/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["Authorization"]
 )
 
 editor = APIRouter(prefix="/api/v1")
