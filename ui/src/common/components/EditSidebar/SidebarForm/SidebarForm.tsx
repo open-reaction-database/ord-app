4fce4ef3581cbeaef0c200bc9a7a9967ff93b042
Daniil Sloboda <daniil_sloboda@epam.com>
2025-02-05 16:37:42 +0400

    Removed debug output

--- ui/src/common/components/EditSidebar/SidebarForm/SidebarForm.tsx
+++ ui/src/common/components/EditSidebar/SidebarForm/SidebarForm.tsx
@@ -45,8 +45,7 @@ export function SidebarForm({ reactionId, reactionPathComponents, isHidden, onFo
     (acc: object, key) => (acc !== null ? Reflect.get(acc, key) || null : null),
     reaction.data,
   );
-  const initialValues: object = new constructor().toObject();
-  console.log(value, initialValues);
+  const initialValues: object = value ?? new constructor().toObject();
   const form = useForm({
     mode: 'controlled',
     initialValues: { ...initialValues },
