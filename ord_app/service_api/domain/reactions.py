d2f62e6d95e3e204a6df2aa48d2486936019d245
Oleksandr Lopatin <lopatin.alexander.y@gmail.com>
2025-02-08 01:01:47 +0100

    handle duplicates on update and create reaction

--- ord_app/service_api/domain/reactions.py
+++ ord_app/service_api/domain/reactions.py
@@ -72,15 +72,12 @@ class ReactionsUseCase:
         return reaction
 
     async def create(self, dataset_id: int, payload: ReactionCreateSchema):
-        insert_data = {"pb_reaction_id": uuid4().hex}
+        pb_reaction = load_message(payload.binpb, Reaction, "binpb")
 
-        if payload.binpb is not None:
-            pb_reaction = load_message(payload.binpb, Reaction, "binpb")
-            if db_reaction := await self.reaction_repo.get(pb_reaction_id=pb_reaction.reaction_id):
-                pb_reaction.reaction_id = f"duplicate-{db_reaction.pb_reaction_id}-{uuid4().hex}"
+        if db_reaction := await self.reaction_repo.get(pb_reaction_id=pb_reaction.reaction_id):
+            pb_reaction.reaction_id = f"duplicate-{db_reaction.pb_reaction_id}-{uuid4().hex}"
 
-            insert_data["binpb"] = pb_reaction.SerializeToString()
-
+        insert_data = {"binpb": pb_reaction.SerializeToString(), "pb_reaction_id": uuid4().hex}
         reaction = await self._create_reaction(dataset_id, insert_data)
         return reaction
 
@@ -91,9 +88,8 @@ class ReactionsUseCase:
             logger.error(f"Failed to read the file dataset_id={dataset_id}, kind={kind}: {e}")
             raise ProtobufDecodeError("An error occurred while reading the file.") from e
 
-        insert_data = {"pb_reaction_id": uuid4().hex, "binpb": pb_reaction.SerializeToString()}
         if db_reaction := await self.reaction_repo.get(pb_reaction_id=pb_reaction.reaction_id):
-            pb_reaction.pb_reaction_id = f"duplicate-{db_reaction.pb_reaction_id}-{uuid4().hex}"
+            pb_reaction.reaction_id = f"duplicate-{db_reaction.pb_reaction_id}-{uuid4().hex}"
 
         insert_data = {"pb_reaction_id": uuid4().hex, "binpb": pb_reaction.SerializeToString()}
         reaction = await self._create_reaction(dataset_id, insert_data)
