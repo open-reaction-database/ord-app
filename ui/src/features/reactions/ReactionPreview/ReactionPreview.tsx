732f2d1e213ebcae41f63402b19b9520a25c48b6
Daniil Sloboda <daniil_sloboda@epam.com>
2025-02-10 21:43:39 +0400

    Implemented reaction preview

---
+++ ui/src/features/reactions/ReactionPreview/ReactionPreview.tsx
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2024 Open Reaction Database Project Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import type { ReactionWrapper } from 'store/entities/reactions/reactions.types.ts';
+import { Fragment, useMemo } from 'react';
+import { renderSvg } from 'common/utils/indigo.ts';
+import type { ord } from 'ord-schema-protobufjs';
+import type { ReactionInputPreview, ReactionProductPreview } from './reactionPreview.types.ts';
+import classes from './reactionPreview.module.scss';
+
+interface ReactionPreviewProps {
+  reaction: ReactionWrapper;
+}
+
+function ReactionProduct({ svg }: Readonly<ReactionProductPreview>) {
+  return (
+    <div
+      className={classes.molecule}
+      dangerouslySetInnerHTML={{ __html: svg || '' }}
+    />
+  );
+}
+
+function ReactionInput({ name, components }: Readonly<ReactionInputPreview>) {
+  return (
+    <div className={classes.inputCard}>
+      <span>{name}</span>
+      <div>
+        {components.map(({ svg }, index) => (
+          <div
+            key={index}
+            className={classes.molecule}
+            dangerouslySetInnerHTML={{ __html: svg || '' }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export function ReactionPreview({ reaction }: Readonly<ReactionPreviewProps>) {
+  const inputs: Array<ReactionInputPreview> = useMemo(() => {
+    return reaction.data.inputs.map(
+      (input): ReactionInputPreview => ({
+        name: input.name,
+        components: (input.components || []).map((component, index) => ({
+          component,
+          svg: reaction.molblocks.inputs[input.name] ? renderSvg(reaction.molblocks.inputs[input.name][index]) : null,
+        })),
+      }),
+    );
+  }, [reaction.data.inputs, reaction.molblocks.inputs]);
+
+  const products = useMemo(() => {
+    return reaction.molblocks.products.map(
+      (molecule, index): ReactionProductPreview => ({
+        product: (reaction.data.outcomes || [])[index] as ord.IReactionOutcome,
+        svg: molecule ? renderSvg(molecule) : null,
+      }),
+    );
+  }, [reaction.molblocks.products, reaction.data.outcomes]);
+
+  return (
+    <div className={classes.wrapper}>
+      {inputs.map((molecule, index) => (
+        <Fragment key={molecule.name}>
+          {index > 0 && index < inputs.length && <span className={classes.plus}>+</span>}
+          <ReactionInput
+            name={molecule.name}
+            components={molecule.components}
+          />
+        </Fragment>
+      ))}
+      <span className={classes.arrow}>⟶</span>
+      {products.map((molecule, index) => (
+        <ReactionProduct
+          key={index}
+          product={molecule.product}
+          svg={molecule.svg}
+        />
+      ))}
+    </div>
+  );
+}
