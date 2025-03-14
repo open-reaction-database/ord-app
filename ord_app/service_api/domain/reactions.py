212eaa83b18d83bc31936fd21d9f37da8a54819b
Alexander Lopatin <lopatin.alexander.y@gmail.com>
2025-03-14 14:49:04 +0100

    Tech/retrun validation info on update (#415)
    * return validation info on update reaction
    
    * return validation info on update reaction

--- ord_app/service_api/domain/reactions.py
+++ ord_app/service_api/domain/reactions.py
@@ -43,7 +43,7 @@ from ord_app.service_api.services.postgresql import get_db_session
 
 async def validate_reaction(reaction: ReactionModel):
     if reaction.binpb is None:
-        return
+        return None, ([], [])
 
     pb_reaction = await run_in_threadpool(load_message, reaction.binpb, Reaction, "binpb")
     try:
@@ -52,8 +52,8 @@ async def validate_reaction(reaction: ReactionModel):
         validation_result = [err], []
 
     if any(validation_result):
-        return False
-    return True
+        return False, validation_result
+    return True, validation_result
 
 
 async def validate_dataset_reactions(db: AsyncSession, dataset_id: int | None = None):
@@ -61,7 +61,8 @@ async def validate_dataset_reactions(db: AsyncSession, dataset_id: int | None = 
     async for reactions in reaction_repo.stream_reactions(chunk_size=1000, dataset_id=dataset_id):
         update_values = []
         for reaction in reactions:
-            update_values.append({"id": reaction.id, "is_valid": await validate_reaction(reaction)})
+            is_valid, _ = await validate_reaction(reaction)
+            update_values.append({"id": reaction.id, "is_valid": is_valid})
         try:
             await reaction_repo.bulk_update(update_values)
         except Exception as err:
@@ -212,6 +213,8 @@ class ReactionsUseCase:
 
     async def get(self, reaction_id):
         if reaction := await self.reaction_repo.get(id=reaction_id):
+            is_valid, (errors, warning) = await validate_reaction(reaction)
+            reaction.validation = {"errors": errors, "warnings": warning}
             return reaction
         raise EntityNotFoundError(f"Reaction with id={reaction_id} not found")
 
@@ -230,14 +233,16 @@ class ReactionsUseCase:
         if duplicated := await self.reaction_repo.get(pb_reaction_id=pb_reaction.reaction_id, dataset_id=dataset_id):
             pb_reaction.reaction_id = f"duplicate-{duplicated.pb_reaction_id}_{uuid4().hex}"
 
+        is_valid, (errors, warning) = await validate_reaction(db_reaction)
         updating_data = {
             "binpb": pb_reaction.SerializeToString(),
             "pb_reaction_id": pb_reaction.reaction_id,
-            "is_valid": await validate_reaction(db_reaction),
+            "is_valid": is_valid,
         }
 
         if reaction := await self.reaction_repo.update(updating_data, id=reaction_id, dataset_id=dataset_id):
             await self.dataset_repo.update_modified_at(dataset_id)
+            reaction.validation = {"errors": errors, "warnings": warning}
             return reaction
 
         raise EntityNotFoundError("Reaction not found")
